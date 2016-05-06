23eea2c3dd2f8a5cfd06f2ada0ae3fe7aca5697a
aashrai <ashrair@gmail.com>
2016-05-06 17:34:45 +0530

    Load filtered applications from cached observable

--- app/src/main/java/aashrai/android/gettowork/model/ApplicationInfoFetcher.java
+++ app/src/main/java/aashrai/android/gettowork/model/ApplicationInfoFetcher.java
@@ -4,6 +4,8 @@ import aashrai.android.gettowork.di.SettingsScope;
 import aashrai.android.gettowork.utils.Utils;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import com.fernandocejas.frodo.annotation.RxLogObservable;
 import java.util.List;
 import javax.inject.Inject;
 import rx.Observable;
@@ -19,13 +21,26 @@ import rx.schedulers.Schedulers;
     this.context = context;
   }
 
-  public Observable<List<ApplicationInfo>> getAllApplications() {
-    return Utils.getAllApplications(context);
+  @RxLogObservable public Observable<List<ApplicationInfo>> getAllApplications() {
+    return Observable.from(
+        context.getPackageManager().getInstalledApplications(PackageManager.GET_META_DATA))
+        .observeOn(Schedulers.computation())
+        .filter(Utils.removeSelfPackage(context))
+        .filter(Utils.removeLaunchers(context))
+        .filter(Utils.removeSystemApps())
+        .toSortedList(Utils.getApplicationSortFunction(context))
+        .observeOn(AndroidSchedulers.mainThread())
+        .cache();
   }
 
-  public Observable<List<ApplicationInfo>> getFilteredApplications(
-      List<ApplicationInfo> applicationInfoList, String query) {
-    return Observable.from(applicationInfoList)
+  public Observable<List<ApplicationInfo>> getFilteredApplications(String query) {
+    return getAllApplications().flatMap(
+        new Func1<List<ApplicationInfo>, Observable<ApplicationInfo>>() {
+          @Override
+          public Observable<ApplicationInfo> call(List<ApplicationInfo> applicationInfoList) {
+            return Observable.from(applicationInfoList);
+          }
+        })
         .observeOn(Schedulers.computation())
         .filter(new PackageQueryFilter(context, query))
         .toList()
