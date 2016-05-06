f8636e7f65d6ff3fadb0090084dc2a00b105b472
aashrai <ashrair@gmail.com>
2016-05-06 17:35:16 +0530

    Removed uneeded globlal states

--- app/src/main/java/aashrai/android/gettowork/model/ApplicationsInfoStore.java
+++ app/src/main/java/aashrai/android/gettowork/model/ApplicationsInfoStore.java
@@ -2,25 +2,18 @@ package aashrai.android.gettowork.model;
 
 import aashrai.android.gettowork.di.SettingsScope;
 import android.content.pm.ApplicationInfo;
-import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
 import rx.Observable;
-import rx.Subscription;
-import rx.functions.Action1;
 import rx.subjects.BehaviorSubject;
 import rx.subjects.Subject;
 
 @SettingsScope public class ApplicationsInfoStore {
 
   private final ApplicationInfoFetcher applicationInfoFetcher;
-  final List<ApplicationInfo> applicationInfoList;
-  private Subject<List<ApplicationInfo>, List<ApplicationInfo>> applicationInfoSubject;
-  private Subscription subjectSubscription;
 
   @Inject public ApplicationsInfoStore(ApplicationInfoFetcher applicationInfoFetcher) {
     this.applicationInfoFetcher = applicationInfoFetcher;
-    applicationInfoList = new ArrayList<>();
   }
 
   public Observable<List<ApplicationInfo>> getInstalledApplications(final String query) {
@@ -28,31 +21,9 @@ import rx.subjects.Subject;
     // report on the main thread since it causes race condition, using PublishSubject also helps
     // with restoring state across configuration changes
 
-    if (subjectSubscription == null || subjectSubscription.isUnsubscribed()) {
-      applicationInfoSubject = BehaviorSubject.create();
-    }
-
-    subjectSubscription = applicationInfoFetcher.getFilteredApplications(applicationInfoList, query)
-        .subscribe(applicationInfoSubject);
+    Subject<List<ApplicationInfo>, List<ApplicationInfo>> applicationInfoSubject =
+        BehaviorSubject.create();
+    applicationInfoFetcher.getFilteredApplications(query).subscribe(applicationInfoSubject);
     return applicationInfoSubject;
-  }
-
-  public Observable<List<ApplicationInfo>> getAllInstalledApplications() {
-    applicationInfoSubject = BehaviorSubject.create();
-    subjectSubscription = applicationInfoFetcher.getAllApplications()
-        .doOnNext(new ApplicationInfoListSetter())
-        .subscribe(applicationInfoSubject);
-    return applicationInfoSubject;
-  }
-
-  private class ApplicationInfoListSetter implements Action1<List<ApplicationInfo>> {
-
-    @Override public void call(List<ApplicationInfo> applicationInfoList) {
-      //This has to be done because we are subscribing to the first observable with all
-      // applications two times
-      if (ApplicationsInfoStore.this.applicationInfoList.isEmpty()) {
-        ApplicationsInfoStore.this.applicationInfoList.addAll(applicationInfoList);
-      }
-    }
   }
 }
